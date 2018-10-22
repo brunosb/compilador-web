a4a2434fd55bdd3276589a7410602d554b6963ee
Bruno Barbosa <barbosa.developer@gmail.com>
2018-10-21 21:28:04 -0300

    Commit initial

---
+++ src/main/java/br/com/uece/compilador/compiladorweb/CompiladorWebApplication.java
@@ -0,0 +1,12 @@
+package br.com.uece.compilador.compiladorweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompiladorWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompiladorWebApplication.class, args);
+    }
+}
