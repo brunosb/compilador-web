11875fbe351c47356103024b1984005f5eaf94cf
Bruno Barbosa <barbosa.developer@gmail.com>
2018-11-22 17:41:05 -0300

    Refatoração do css e javascript; Style para o Input upload; Ajustes em geral.

--- src/main/resources/static/js/load-file.js
+++ src/main/resources/static/js/load-file.js
@@ -48,3 +48,25 @@ function compilar(file) {
         }
     });
 }
+
+var inputs = document.querySelectorAll( '.inputfile' );
+Array.prototype.forEach.call( inputs, function( input )
+{
+    var label = input.nextElementSibling, labelVal = label.innerHTML;
+
+    input.addEventListener( 'change', function( e )
+    {
+        var fileName = e.target.value.split( '\\' ).pop();
+
+        if( fileName )
+            label.querySelector( 'span' ).innerHTML = fileName + '  ';
+        else
+            label.innerHTML = labelVal;
+
+        loadFileAsText();
+    });
+
+    // Firefox bug fix
+    input.addEventListener( 'focus', function(){ input.classList.add( 'has-focus' ); });
+    input.addEventListener( 'blur', function(){ input.classList.remove( 'has-focus' ); });
+});
